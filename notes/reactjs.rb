6791635518201578bc25653d7fc1bfb9d6fc7b02
Arto Bendiken <arto@bendiken.net>
2016-04-17 01:02:48 +0200

    Added notes re: React.js and React Native.

---
+++ notes/reactjs.rb
@@ -0,0 +1,15 @@
+****************************************************************
+Arto's Notes re: `ReactJS <https://facebook.github.io/react/>`__
+****************************************************************
+
+Introductions
+=============
+
+* `React Demystified
+  <http://blog.reverberate.org/2014/02/react-demystified.html>`__ (2014)
+
+React Native
+============
+
+* `React.js Conf 2015 Keynote - Introducing React Native`
+  <http://youtu.be/KVZ-P-ZI6W4>`__
